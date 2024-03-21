fac087f3d7b99f31beba131c2ddfd50f446b2e8e
Mohammed Waris <mohammedwaris60@gmail.com>
2024-03-22 00:29:12 +0530

    Debugged Login mistake

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,182 +1,74 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
-import noteContext from "../context/notes/noteContext";
-import AddNote from "./AddNote";
-import NoteItem from "./NoteItem";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const Notes = (props) => {
-  // “useContext” hook is used to create common data that can be accessed throughout the component hierarchy without passing the props down manually to each level. Context defined will be available to all the child components without involving “props”.
-  const context = useContext(noteContext);
-  let navigate = useNavigate();
-  const { notes, getNotes, editNote } = context;
+const Login = () => {
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    if (localStorage.getItem("token")) {
-      getNotes();
-    } else {
-      navigate("/");
-    }
-    // eslint-disable-next-line
-  }, []);
-
-  const ref = useRef(null);
-  const refClose = useRef(null);
-  const [note, setNote] = useState({
-    id: "",
-    etitle: "",
-    edescription: "",
-    etag: "default",
-  });
-
-  const updateNote = (currentNote) => {
-    ref.current.click();
-    setNote({
-      id: currentNote._id,
-      etitle: currentNote.title,
-      edescription: currentNote.description,
-      etag: currentNote.tag,
+  const handleSubmit = async (e) => {
+    e.preventDefault(); //else page will reload
+    //connection with api
+    const response = await fetch("http://localhost:5000/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: credentials.email,
+        password: credentials.password,
+      }),
     });
-  };
-
-  const handleClick = (e) => {
-    editNote(note.id, note.etitle, note.edescription, note.etag);
-    refClose.current.click();
-    props.showAlert("Note Updated Successfully", "success");
+    const json = await response.json();
+    console.log(json);
+    if (json.success) {
+      // Save the auth token and redirect
+      localStorage.setItem("token", json.authtoken);
+      navigate("/"); // Use navigate instead of history.push
+    } else {
+      alert("Invalid credentials");
+    }
   };
 
   const onChange = (e) => {
-    setNote({ ...note, [e.target.name]: e.target.value });
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
   };
 
-  // In this, basically there are 3 segments
-  // In first segment, there is AddNote functionality where user can add a new note
-  // In second segment, there is edit note functionality which will open only when user clicks on edit button
-  // In third segment, I am showing all the notes of a user
   return (
-    <div className="container">
-      <AddNote showAlert={props.showAlert} />
-      <button
-        style={{ display: "none" }}
-        type="button"
-        className="btn btn-primary"
-        ref={ref}
-        data-bs-toggle="modal"
-        data-bs-target="#exampleModal"
-      >
-        Submit
-      </button>
-
-      <div
-        className="modal fade"
-        id="exampleModal"
-        tabIndex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">
-                Edit Note
-              </h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div className="modal-body">
-              <form className="mb-5 mt-4">
-                <div className="mb-3">
-                  <label htmlFor="etitle" className="form-label">
-                    Title
-                  </label>
-                  <input
-                    value={note.etitle}
-                    type="text"
-                    className="form-control"
-                    id="etitle"
-                    name="etitle"
-                    aria-describedby="emailHelp"
-                    onChange={onChange}
-                    minLength={5}
-                    required
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="edescription" className="form-label">
-                    Description
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="edescription"
-                    value={note.edescription}
-                    name="edescription"
-                    onChange={onChange}
-                    minLength={5}
-                    required
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="etag" className="form-label">
-                    Tag
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="etag"
-                    value={note.etag}
-                    name="etag"
-                    onChange={onChange}
-                    minLength={5}
-                  />
-                </div>
-              </form>
-            </div>
-            <div className="modal-footer">
-              <button
-                ref={refClose}
-                type="button"
-                className="btn btn-secondary"
-                data-bs-dismiss="modal"
-              >
-                Close
-              </button>
-              <button
-                disabled={
-                  note.etitle.length < 5 || note.edescription.length < 5
-                }
-                onClick={handleClick}
-                type="button"
-                className="btn btn-primary"
-              >
-                Update Note
-              </button>
-            </div>
-          </div>
+    <div className="container my-4">
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            value={credentials.email}
+            onChange={onChange}
+            aria-describedby="emailHelp"
+          />
         </div>
-      </div>
-
-      <div className="row">
-        <h2 className="my-3 text-warning">Your Notes</h2>
-        <div className="container mx-1">
-          {notes.length === 0 && "No notes to display"}
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            value={credentials.password}
+            onChange={onChange}
+            className="form-control"
+            id="password"
+            name="password"
+          />
         </div>
-        {notes.map((note) => {
-          return (
-            <NoteItem
-              key={note._id}
-              updateNote={updateNote}
-              showAlert={props.showAlert}
-              note={note}
-            />
-          );
-        })}
-      </div>
+        <button type="submit" className="btn btn-primary">
+          Login
+        </button>
+      </form>
     </div>
   );
 };
 
-export default Notes;
+export default Login;
